cd320dbfdbe198d5a34f881caaae3dd0bcf39783
Yash-kaushik2002 <116297808+Yash-kaushik2002@users.noreply.github.com>
2022-10-20 22:01:25 +0530

    Create equal.cpp

---
+++ equal.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    
+    if(a==b)
+    cout<<"NUMBERS ARE EQUAL";
+    
+    else 
+    cout<<"NUMBERS ARE NOT EQUAL";
+
+    return 0;
+}
